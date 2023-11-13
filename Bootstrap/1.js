8dcf24ea4797701883366254240c06277efa3b3e
Zeynel Basarane <zeynelbasaran@hotmail.com>
2023-11-13 21:37:35 +0300

    11

---
+++ Bootstrap/1.js
@@ -0,0 +1,12 @@
+let zaman = new Date ()
+console.log(zaman)
+
+console.log(zaman.getFullYear())
+console.log(zaman.getDay())
+
+
+let birtday = new Date (1992,6,26);
+console.log(zaman.getFullYear() - birtday.getFullYear())
+console.log(zaman.getMonth( - birtday.getMonth()))
+
+console.log(zaman.getSeconds)
