7f8386318333b90c44d9b8794a999f19c4b57e55
Zeynel Basarane <zeynelbasaran@hotmail.com>
2023-12-01 10:23:41 +0300

    ekleme

--- JS EGİTİM/5Numbers.js
+++ JS EGİTİM/5Numbers.js
@@ -25,4 +25,17 @@ console.log(veri1)
 veri1 =sayi.toFixed(5);  //Virgülden sonraki 3 rakamı alır
 console.log(veri1)
 
-veri1 = Math.PI;
+veri1 = Math.PI; //Pi sayısını kullanmak için
+veri1 = Math.round(); //Ondalıklı sayıyı yuvarlama yakın yere
+veri1 = Math.ceil();  //Ondalıklı sayıyı Yukarı yuvarlar 
+veri1 = Math.floor(); //Ondalıklı sayıyı Aşagı yuvarlar
+veri1 = Math.pow(3,3) //3ün karesini almak için
+veri1 = Math.sqrt(81)  //Karekök almak için
+veri1 = Math.abs(-50); //Mutlak değer alır
+veri1 = Math.min(1,7,9,4,20) // En küçük değeri alır
+veri1 = Math.max(1,7,9,4,20); // En büyük değeri alır
+veri1 = Math.random(); // 0-1 Arası rasgele sayı
+veri1 = Math.random()*10 // 0-10 Arasından rastgele
+veri1 = Math.floor(Math.random()*100);//0-100 Arasından 
+veri1 = Math.floor(Math.random()*100+1);// 1-100 Arasından 
+
