c09b20b386fc66a48a7df705741f78c36299afa7
Zeynel Basarane <zeynelbasaran@hotmail.com>
2023-12-07 00:03:00 +0300

    7.12.2023

---
+++ JS EGİTİM/14WindowObjesi.js
@@ -0,0 +1,28 @@
+//Window objesi
+console.log(window)
+
+//Alert metodu  (Uyarı mesajı vermek için)
+//alert("Uyarı mesajı")
+
+//Prompt Metodu (Kullanıcıdan bilgi almak)
+//let adSoyadı=prompt("Adınızı Soyadınızı Giriniz.")  
+
+//Confirm metodu (Evet-Hayır Penceresi)
+//veri3 = confirm(`Çıkmak istediginize emin misiniz ?`);
+//if (veri3) { 
+//    console.log(`Çıkış yapıldı`) }
+//else{
+//    console.log(`Çıkış yapılmadı`)
+//}
+
+//Location metodu (Tarayıcının şuanki özelliklerini gösterir)
+veri2 = window.location;
+console.log(veri2);
+veri2=location.href;
+console.log(veri2);
+veri2=location.host;
+console.log(veri2);
+//veri.href = "https://www.youtube.com/"; Web sitesine yönlendirme
+//window.location.reload();    //Sayfa sürekli yeniden yüklenir
+//window.navigator;  Tarayıcının özelliklerini gösterir
+//window.document;   Consola yazıldıgında sayfanın HTML sini döker
