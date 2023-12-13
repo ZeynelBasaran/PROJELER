15abad55a162df9051ee7bb7dd226a5942f6e626
Zeynel Basarane <zeynelbasaran@hotmail.com>
2023-12-13 15:35:53 +0300

    12.13.23

---
+++ Projeler/ToDoList/todolist.js
@@ -0,0 +1,13 @@
+
+const newTask= document.querySelector("#btnAddNewTask")
+const text= document.querySelector("#txtTaskName")
+const deleteAll= document.querySelector("#btnDeleteAll")
+const taskList= document.querySelector("#task-list")
+
+addEventListener()
+
+function addEventListener(){
+    
+    console.log("submit")
+
+}
