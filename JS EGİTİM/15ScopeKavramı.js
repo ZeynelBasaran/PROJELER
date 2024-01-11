2b0963548a5d38830c541155180e9896ea57a484
Zeynel Basarane <zeynelbasaran@hotmail.com>
2024-01-11 15:05:02 +0300

    11.02.24

--- JS EGİTİM/15ScopeKavramı.js
+++ JS EGİTİM/15ScopeKavramı.js
@@ -1,26 +1,34 @@
+/*
+scope = kapsam
+var => functon scope => global scope (Block içindede olsa ulaşılabilir)
+let => block scope  (Block içinde kaldımı erişilemez)
+const => block scope
+https://www.youtube.com/watch?v=kjLv-sIyVa8
+*/
 
-//Glabal Scope
-var veri1 = 5;  // Global Scope
+
+/*              GLOBAL SCOPE
+var veri1 = 5;   //Global Scope
 let veri2 = 10;
 const veri3 = 15;
-//
-//function fonksiyon1() {
-//    var veri1 = 30;  // Fonksiyon içerisindeki veriler dışarıyı ezdi içerideki 
-//    let veri2 = 60;   //veriyi baz aldı.
-//    const veri3 = 90;
-//    console.log(veri1, veri2, veri3)
-//}
-//fonksiyon1()
-//console.log(veri1, veri2, veri3);  //Buradaki veri ise Global Scope'dan
 
+function fonksiyon1() {
+    var veri1 = 30;   //Fonksiyon içerisindeki veriler dışarıyı ezdi içerideki 
+    let veri2 = 60;  // veriyi baz aldı.
+    const veri3 = 90;
+    console.log(veri1, veri2, veri3)
+}
+fonksiyon1()
+console.log(veri1, veri2, veri3);  //Buradaki veri ise Global Scope'dan
+*/
 
-//Block Scope (bir degişkenin hem block için hem dışı için VAR kullanılır)
-//if (true) {
-//    var a = 10;
-//    let b = 20; 
-//    const c = 30;
-//}
-//console.log(a) // Var ile oluşturulan değer block dışındada sürmekte.
+//             BLOCK SCOPE (bir degişkenin hem block için hem dışı için VAR kullanılır)
+if (true) {
+    var a = 10;
+    let b = 20; 
+    const c = 30;
+}
+console.log(a)  //Var ile oluşturulan değer block dışındada sürmekte ve degeri almakta
 //console.log(b) //Block dışında var olamıyor
 //console.log(c) //Block dışında var olamıyor
 
@@ -30,4 +38,4 @@ if (true) {
     const veri3 = 30;
   
 }
-console.log(veri1, veri2, veri3);//Veri2veVeri3 Globaldan veri1 ise 10 olarak geldi. Var ile oluşturulan değer en son degişkenden gelir.
+console.log(veri1, veri2, veri3); //Veri2veVeri3 Globaldan veri1 ise 10 olarak geldi. Var ile oluşturulan değer en son degişkenden gelir.
