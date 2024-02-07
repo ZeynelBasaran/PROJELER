8983c0ef04e5e8e4f76cd6aa3130fc6aa7c14ffe
Zeynel Basarane <zeynelbasaran@hotmail.com>
2024-02-07 11:27:42 +0300

    7.2.24

--- 50Günde50Proje/1AnimasyonluAçılanKart.js
+++ 50Günde50Proje/1AnimasyonluAçılanKart.js
@@ -1,1 +1,23 @@
 
+let panels = document.querySelectorAll(".panel")
+
+
+panels.forEach((panel) => {
+    panel.addEventListener("click", addActive)
+})
+
+function addActive(click) {
+    console.log(click.target);
+    removeActive();
+    click.target.parentElement.classList.add("active")
+
+}
+
+function removeActive() {
+    panels.forEach((panel) => {
+        panel.classList.remove("active");
+        console.log(panel)
+    })
+}
+
+
--- 50Günde50Proje/2İlerlemeAdimları.js
+++
@@ -1,7 +0,0 @@
-const progress = document.getElementById("progress")
-const next = document.getElementById("next")
-const back = document.getElementById("back")
-const circle = document.querySelectorAll(".circle")
-
-let currentActive =1;
-next.addEventListener("click")
