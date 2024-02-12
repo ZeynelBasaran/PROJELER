fef7682ba05f40dd78f0561df25be53df6db1ad5
Zeynel Basarane <zeynelbasaran@hotmail.com>
2024-02-12 22:52:01 +0300

    12.2.24

--- JS EGİTİM/10Objectliterals.js
+++ JS EGİTİM/10Objectliterals.js
@@ -1,4 +1,12 @@
+//Objeler, içerisinde birden fazla değeri depolayan yapılardır. Property olarak bilinen bu değerler primitive(String,number, boolean vb.) veya başka objeler olabilirler ve her bir değer string veya sembol ile isimlendirilebilir.
+//Objeler değiştirilebilen veri tiplerindendir. İçerdiği propertyler, objenin kendisi const keyword ile atanmış olsa bile değiştirilebilirler.
+//OBJENİN AVANTA JI KEY VALUE ŞEKLİNDE OLMASINDAN GELİR
 
+let array01 = ["1","2","abc"]
+let obj = {obj:1}
+
+console.log("array01", typeof(array01))
+console.log("obj", typeof(obj))
 let user = {
     userName: "levent",
     firstName: "Ertuna",
@@ -14,3 +22,25 @@ console.log(veri)
 
 veri = user.age
 console.log(veri)
+
+user.age= 23; // Bir veri atamak için eşittir operatörü kullanılır.
+console.log(user)
+
+keys=Object.keys(user) // Sadece key bilgilerine ulaşabiliriz.
+console.log(keys)
+console.log(Object.values(user)) // Sadece valuelere ulaşmak
+
+keys.forEach(element => { // Key bilgilerini for each ile dönmek.
+    console.log("keys",element) // Tek tek keyleri yazdırmak
+    console.log("value ", user[element])// Tek tek value yazdırmak
+});
+
+var person = {
+	name: 'Rumeysa',
+	surname: 'Turgut',
+	age: 23,
+	shortName: function (){
+        return `Benim adım ${this.name} ve ben ${this.age}dayım`//Fonksiyon ekleyip çalıştırabiliriz.
+    }
+};
+console.log(person.shortName())
