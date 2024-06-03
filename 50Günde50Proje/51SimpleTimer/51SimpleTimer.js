57ea07cf90fb64eb07d9f907667c711f170dc638
Zeynel <zeynelbasaran@hotmail.com>
2024-06-04 02:16:20 +0300

    51. Proje Ekleme

--- 50Günde50Proje/51SimpleTimer/51SimpleTimer.js
+++ 50Günde50Proje/51SimpleTimer/51SimpleTimer.js
@@ -1,29 +1,120 @@
-const play = document.querySelector(`.fa-play`)
-const refresh = document.querySelector(`.fa-sync`)
+const play = document.querySelector(`#play`)
+const refresh = document.querySelector(`#reset`)
+const root = document.querySelector(":root")
+const timer = document.querySelector("#timer")
 
-play.addEventListener("click",geriSayim)
 
-function geriSayim(){
+
+let saniye = 10
+let playing = false;
+let currentSeconds = saniye;
+timer.innerText = formatTime(saniye)
+
+const timerInterval = setInterval(run, 1000)
+
+
+//Zaman formatlama fonksiyonu
+function formatTime(seconds) {
+    const minutes = Math.floor(seconds / 60)
+    const newSeconds = seconds % 60;
+
+    //padStart Metodu Sayıları belirli bir uzunluğa getirmek (örneğin tarih ve saat formatlama)
+    return `${minutes.toString().padStart(2, "0")}:${newSeconds.toString().padStart(2, "0")}`;
+}
+
+//Play Tuşu
+play.addEventListener("click", () => {
     
+    playing = !playing
+    play.classList.toggle("play")
+    play.classList.toggle("bg-green-500")
+
+    let playIcon = play.querySelector("i")
+    playIcon.classList.toggle("fa-play")
+    playIcon.classList.toggle("fa-pause")
+
+})
+
+//GeriSayım ve puase kontrol fonk.
+function run() {
+    if (playing) {
+        currentSeconds -= 1;
+        if (currentSeconds <= 0){
+            clearInterval(timerInterval)
+            resetAll()
+         }
+    }
+    
+    timer.innerText = formatTime(currentSeconds)
+    root.style.setProperty("-degress",calcDeg())
 }
 
 
-function saat() {//Güncel Saati Çekip DOM'a ekleme
-    const zaman = new Date()
-    const dakika = zaman.setSeconds()
-    const saniye = zaman.setMinutes(10)
-
-    
-   // if (dakika < 10) dakika = "0" + dakika;
-   // if (saat < 10) saat = "0" + saat;
-
-    // const clock = document.querySelector(`.clock`).innerHTML = 
-
+function calcDeg(){
+    return`${360-((currentSeconds/saniye)*360)}deg`;
 }
 
-setInterval(() => { saat() }, 1000);//Her saniye çalıştırmak için 
+//Zamanlayıcıyı tekrar kurmak için
+refresh.addEventListener("click", resetAll)
 
-var date = new Date();
-date.setMinutes(10, 0, 0);
+function resetAll(){
+    playing = false
 
-console.log(date)
+    timer.innerText = formatTime(saniye)
+    currentSeconds = saniye;
+
+    let playIcon = play.querySelector("i")
+    play.classList.remove("bg-green-500")
+    play.classList.remove("play")
+    playIcon.classList.add("fa-play")
+    playIcon.classList.remove("fa-pause")
+    root.style.setProperty("--degress","0deg")
+}
+
+
+
+
+
+
+
+/*
+//1Girilen Saniyeye göre geri Sayım sayacının oluşturulması
+function geriSayim() {
+    let m = Math.floor(saniye / 60)
+    let s = saniye % 60
+    m = m < 10 ? `0${m}` : m
+    s = s < 10 ? `0${s}` : s
+    timer.innerHTML = `${m}:${s}`
+    if (saniye <= 0){saniye = 0}
+    saniye--
+    console.log(saniye)
+
+
+}
+geriSayim()
+
+//Play Tuşu
+play.addEventListener("click", () => {
+    
+    setInterval(geriSayim, 1000)
+    play.style.display = "none"
+    pause.style.display = "block"
+})
+
+pause.addEventListener("click", () => {
+    clearInterval()
+})
+
+/*
+//Zamanlayıcıyı tekrar kurmak için
+refresh.addEventListener("click", () => {
+    clearInterval()
+   
+
+})
+
+
+
+
+
+*/
